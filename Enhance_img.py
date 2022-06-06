29394a9b5c46e753f3d09710cb132f08e9f1bf11
YoussefTurkey <85898292+YoussefTurkey@users.noreply.github.com>
2022-06-06 16:45:35 +0200

    Inserting sample enhanced images

--- Enhance_img.py
+++ Enhance_img.py
@@ -7,33 +7,33 @@ for i in range(5):
     print('Image is loading...')
     img = Image.open(f'img_{i+1}.jpg')
     stack.append(img)
-print(stack)
+# print(stack)
 for j in range(5):
     stack[j].show()
 
 # Converting grayscale image to binary image
-stack_Binary = []
-for i in range(5):
-    print('Converting grayscale image to binary image is loading...')
-    img_gray = stack[i].convert('L')
-    binary_img = img_gray.point(lambda x: 0 if x < 128 else 255, '1')
-    stack_Binary.append(binary_img)
+# stack_Binary = []
+# for i in range(5):
+#     print('Converting grayscale image to binary image is loading...')
+#     img_gray = stack[i].convert('L')
+#     binary_img = img_gray.point(lambda x: 0 if x < 128 else 255, '1')
+#     stack_Binary.append(binary_img)
 
-# Removing a background of an image
-stack_BG = []
-for j in range(5):
-    print('Removing a background of an image is loading...')
-    img_BG = stack[i].convert('RGBA')
-    for item in img_BG.getdata():
-        if item[:3] == (255, 255, 255):
-            stack_BG.append((255, 255, 255, 0))
-        else:
-            stack_BG.append(item)
-    img_BG.putdata(stack_BG)
-for k in range(5):
-    stack_BG[k].show()
-for m in range(5):
-    new_img_without_BG = stack_BG[m].save(f'img_{i+1}_without_BG.jpg')
+# # Removing a background of an image
+# stack_BG = []
+# for j in range(5):
+#     print('Removing a background of an image is loading...')
+#     img_BG = stack[i].convert('RGBA')
+#     for item in img_BG.getdata():
+#         if item[:3] == (255, 255, 255):
+#             stack_BG.append((255, 255, 255, 0))
+#         else:
+#             stack_BG.append(item)
+#     img_BG.putdata(stack_BG)
+# for k in range(5):
+#     stack_BG[k].show()
+# for m in range(5):
+#     new_img_without_BG = stack_BG[m].save(f'img_{i+1}_without_BG.jpg')
 
 # Enhancing images by soomthing
 stack_Smooth = []
